9cbb429e4b6673c5ad86bd4ed865571667cd33fd
Vitor Dino <vitor@kunstdesign.com.br>
2018-10-18 10:17:47 -0300

    :sparkles::memo:

---
+++ src/LightCard.js
@@ -0,0 +1,53 @@
+const React = require('react')
+
+const containerStyle = {
+	width: '16rem',
+	height: '32rem',
+	fontFamily: 'Input Sans, sans-serif',
+	textRendering: 'optimizeLegibility',
+	fontSmoothing: 'antialiased',
+	WebkitFontSmoothing: 'antialiased',
+	overflow: 'hidden',
+	display: 'flex',
+	flexDirection: 'column',
+	justifyContent: 'center',
+	boxSizing: 'border-box',
+}
+
+const wrapperStyle = {
+	background: '#fff',
+	color: '#0B0B0B',
+	boxSizing: 'border-box',
+	fontSize: '0.75rem',
+	lineHeight: '1.125rem',
+}
+
+const textStyle = {
+	padding: '1.125rem',
+}
+
+
+const leftPad = n => (''+n).length !== 2 ? '0'+n : n
+const mapPath = () => `http://localhost:5000/maps/${leftPad(Math.floor(Math.random() * 31))}.png`
+
+module.exports = ({
+	media = "catly",
+	text = "Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.",
+	image = 'https://valevets.com/wp-content/uploads/2014/08/kitten1.jpg',
+}) => (
+	<div style={containerStyle}>
+		<div style={wrapperStyle}>
+			{!!media && (
+				<img src={`http://localhost:5000/media/${media}.svg`} style={{width: '100%', display: 'block'}}/>
+			)}
+			{!!image && (
+				<img src={image} style={{width: '100%', display: 'block'}}/>
+			)}
+			{!!text && (
+				<div style={textStyle}>
+					{text}
+				</div>
+			)}
+		</div>
+	</div>
+)
--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 const profiles = require('./profiles-test.json')
-const App = require('./DarkCard')
+const DarkCard = require('./DarkCard')
+const LightCard = require('./LightCard')
+
+const wrapperStyle = {
+	display: 'flex',
+	alignItems: 'center',
+	flexWrap: 'wrap',
+	justifyContent: 'space-around',
+	minWidth: '90vw'
+}
 
 ReactDOM.render(
-	<App {...profiles[Math.floor(Math.random()*profiles.length)]}/>,
+	<div style={wrapperStyle}>
+		<DarkCard {...profiles[Math.floor(Math.random()*profiles.length)]}/>
+		<LightCard {...profiles[Math.floor(Math.random()*profiles.length)]}/>
+	</div>,
 	document.getElementById('root')
 )
