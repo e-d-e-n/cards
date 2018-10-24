c264dd438fd76a54586ebb61e1af5a96a623d894
Leonardo Dino <leonardo@kunstdesign.com.br>
2018-10-24 17:35:37 -0200

    save module

--- scripts/saveFB.js
+++ scripts/saveFB.js
@@ -1,35 +1,13 @@
-const fs = require('fs')
-const crypto = require('crypto')
-const repng = require('repng')
-const Component = require('../src/components/FBCard/index')
-const Parallel = require('async-parallel')
+#!/usr/bin/env node
+
+const repng = require('repng') // babel-register
+const {each} = require('async-parallel')
 const data = require('../public/media/data.json')
-
-const options = {
-	width: 512,
-	height: 1024,
-	css: 'html {font-size: 32px}',
-}
-
-const createFileName = obj => 'output/fb/' + crypto
-	.createHash('md5')
-	.update(JSON.stringify(obj))
-	.digest('hex') + '.png'
-
-let index = 0
-
-const render = async props => {
-	const thisJobIndex = ++index
-	console.time(`job #${thisJobIndex}`)
-	const result = await repng(Component, {...options, props})
-	const file = fs.createWriteStream(createFileName(props))
-	const stream = result.pipe(file)
-	await new Promise(resolve => stream.on('finish', resolve))
-	console.timeEnd(`job #${thisJobIndex}`)
-}
+const component = require('../src/components/FBCard')
+const render = require('../lib/save')('fb', component)
 
 !(async () => {
-	console.time(`all jobs`)
-	await Parallel.each(data, render, 16)
-	console.timeEnd(`all jobs`)
+	console.time('all jobs')
+	await each(data, render, 16)
+	console.timeEnd('all jobs')
 })()
--- scripts/saveLight.js
+++ scripts/saveLight.js
@@ -1,35 +1,13 @@
-const fs = require('fs')
-const crypto = require('crypto')
-const repng = require('repng')
-const Component = require('../src/components/LightCard/index')
-const Parallel = require('async-parallel')
-const data = require('../public/media/data.json')
+#!/usr/bin/env node
 
-const options = {
-	width: 512,
-	height: 1024,
-	css: 'html {font-size: 32px}',
-}
-
-const createFileName = obj => 'output/light/' + crypto
-	.createHash('md5')
-	.update(JSON.stringify(obj))
-	.digest('hex') + '.png'
-
-let index = 0
-
-const render = async props => {
-	const thisJobIndex = ++index
-	console.time(`job #${thisJobIndex}`)
-	const result = await repng(Component, {...options, props})
-	const file = fs.createWriteStream(createFileName(props))
-	const stream = result.pipe(file)
-	await new Promise(resolve => stream.on('finish', resolve))
-	console.timeEnd(`job #${thisJobIndex}`)
-}
+const repng = require('repng') // babel-register
+const {each} = require('async-parallel')
+const data = require('../public/profiles/data.json')
+const component = require('../src/components/LightCard')
+const render = require('../lib/save')('light', component)
 
 !(async () => {
-	console.time(`all jobs`)
-	await Parallel.each(data, render, 16)
-	console.timeEnd(`all jobs`)
+	console.time('all jobs')
+	await each(data, render, 16)
+	console.timeEnd('all jobs')
 })()
